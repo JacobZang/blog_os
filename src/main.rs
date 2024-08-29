81844fcc21e89d8edbd5b0847f4196e2d98a0f11
Jacobe <jacobe.zang@gmail.com>
2024-08-29 20:56:25 +0800

    src: add test and exit code
    Signed-off-by: Jacobe <jacobe.zang@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 #![no_std] // 不链接 Rust 标准库
 #![no_main] // 禁用所有 Rust 层级的入口点
 
@@ -8,6 +11,9 @@ mod vga_buffer;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -16,4 +22,31 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
